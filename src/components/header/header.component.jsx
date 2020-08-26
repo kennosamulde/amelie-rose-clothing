81734b37fb6dcaeaa02d69a8043a52ac1ef7a3c4
kenno <kennosamulde@yahoo.com.ph>
2020-08-26 17:20:24 +0800

    implemented redux on header and app component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,38 +1,55 @@
-import React from "react"
+import React, { useState } from "react"
 import "./header.styles.scss"
 import { Link } from "react-router-dom"
 import { auth } from "../../firebase/firebase.utils"
+// lets our component have access to reducers/redux
 import { connect } from "react-redux"
 
-const Header = ({ currentUser }) => (
-  <div className="header">
-    <div className="options">
-      <Link className="option" to="/shop">
-        SHOP
-      </Link>
-      <Link className="option" to="/shop">
-        CONTACT
-      </Link>
-    </div>
-    <Link className="logo-container" to="/">
-      <h1 className="logo">Amelie Rose</h1>
-    </Link>
-    <div className="options">
-      {currentUser ? (
-        <div className="option" onClick={() => auth.signOut()}>
-          SIGN OUT
-        </div>
-      ) : (
-        <Link className="option" to="/signIn">
-          SIGN IN
-        </Link>
-      )}
-    </div>
-  </div>
-)
+const Header = ({ currentUser }) => {
+  const [header, setHeader] = useState(false)
 
+  const changeBackground = () => {
+    if (window.scrollY >= 80) {
+      setHeader(true)
+    } else {
+      setHeader(false)
+    }
+  }
+
+  window.addEventListener("scroll", changeBackground)
+
+  return (
+    <div className={header ? "header scrolled" : "header"}>
+      <div className="options">
+        <Link className="option" to="/shop">
+          SHOP
+        </Link>
+        <Link className="option" to="/shop">
+          CONTACT
+        </Link>
+      </div>
+      <Link className="logo-container" to="/">
+        <h1 className="logo">Amelie Rose</h1>
+      </Link>
+      <div className="options">
+        {currentUser ? (
+          <div className="option" onClick={() => auth.signOut()}>
+            SIGN OUT
+          </div>
+        ) : (
+          <Link className="option" to="/signIn">
+            SIGN IN
+          </Link>
+        )}
+      </div>
+    </div>
+  )
+}
+
+// allows us to access the state being our root-reducer
 const mapStateToProps = (state) => ({
   currentUser: state.user.currentUser,
 })
 
+// connects our reducers/state to this component
 export default connect(mapStateToProps)(Header)
